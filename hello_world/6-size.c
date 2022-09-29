cc5fdac628057ce4602dd575231ff0d7c1547bdd
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-09-29 18:17:09 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Print a string
+ *
+ * Description: Print the size of various types
+ *
+ * Return Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	char charType;
+	long longType;
+	long long LONGType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType);
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType);
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType);
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(LONGType);
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType);
+
+	return (0);
+}

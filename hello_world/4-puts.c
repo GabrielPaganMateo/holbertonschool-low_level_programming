ef23c6bace8e225ba7e36c8e93b74553e331f132
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-09-29 17:22:49 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@ int main()
 
 {
 
-char firstputs[] = "Programming is like building a multilingual puzzle.";
+char firstputs[] = "\"Programming is like building a multilingual puzzle";
 
 puts(firstputs);
 

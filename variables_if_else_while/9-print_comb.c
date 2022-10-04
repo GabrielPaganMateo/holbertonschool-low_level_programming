dd14f7f9c8a7658707c58db62d8d4c3a63d7c6f0
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 13:57:17 +0000

    combination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- Prints numbers
+ *
+ * Return: All possible combinations of single digit #'s
+ */
+int main(void)
+{
+	int pr0n;
+
+	for (pr0n = '0' ; pr0n <= '9' ; pr0n++)
+	{
+		putchar(pr0n);
+		
+		if (pr0n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

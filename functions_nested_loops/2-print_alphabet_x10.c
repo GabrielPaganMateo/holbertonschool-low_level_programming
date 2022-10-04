3b35ee4a61f977620bd1dbd5b38a7c666d26db2b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 17:37:59 +0000

    10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- Print letters
+ *
+ * Return: Alphabet ten times, in lowercase
+ */
+int main (void)
+{
+	int ti;
+	char al;
+
+	for (ti = 0 ; ti <= 10 ; ti++)
+	{
+		for (al = 'a' ; al <= 'z' ; al++)
+		{
+			print_alphabet()
+		}
+	}
+}

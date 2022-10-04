4c0a860ea4c1b9b5befd59668a55553c48b17bf9
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 18:40:26 +0000

    parameter

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - Check for lowercase character
- * @parameterc: c is any lowercase letter
+ * @c: c is any lowercase letter
  *
  * Return: 1 if c is lowercase or 0 otherwise
  */
 int _islower(int c)
-{	
+{
 	if (c <= 'z' && c >= 'a')
 	{
 		return (1);

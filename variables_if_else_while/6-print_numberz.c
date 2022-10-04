d70eb4402ace5dea3d3135d97ae0aecb9ae526d8
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 11:39:33 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Print numbers with putchar
+ *
+ * Return: Single digit numbers with putchar
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0 ; dig <= 9 ; dig++)
+	{
+		putchar(dig);
+	}
+	putchar('\n');
+	return (0);
+}

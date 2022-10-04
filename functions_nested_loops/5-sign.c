c1f00b6985111856f85b774336bf1f906e7fa1a5
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 21:32:33 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign- Prints the sign of a number
- *
+ * @n: number that will be checked 
  * Return: 1 if n is greater than zero, 0 if zero and -1 if less than zero
  */
 int print_sign(int n)
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);

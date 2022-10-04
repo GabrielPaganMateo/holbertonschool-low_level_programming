2cf1ad1f0c60a5995fbe352c078a4d30dcdb97e9
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-04 23:03:10 +0000

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit- Prints last digit of a number
+ * @n: last digit of number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10
+
+	while (n <= 0 && n >= 0)
+	{
+		return (x);
+	}
+}

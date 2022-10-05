50211c287dd665d042754d57db9dfb79235bcfaa
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-05 01:10:13 +0000

    wow

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,27 @@
  */
 int print_last_digit(int n)
 {
-	unsigned int x, y;
-	
-	x = n % 10;
-	y = n % 10;
+	int x;
 
-	return (y);
-	return (x);
+		
+	x = n % 10;
+	
+	if (x < 0)
+	{
+		x = n * (-1);
+		_putchar(x + x);
+		_putchar(' ');
+	}
+	else if (x > 0)
+	{
+		_putchar(x + x);
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar(x + x);
+		_putchar(' ');
+	}
+
+	return (x + x);
 }

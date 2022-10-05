0f666a1bab90754ac7bbc929aead8c9fc2bb25a2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-05 15:19:23 +0000

    24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,15 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
-	
-	for (a = 0 ; a <= 2 ; a++)
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
 	{
-		_putchar(a);
-		
-		for (b = 0 ; b <= 4; b++)
+		for (min = 0 ; min <= 59 ; min++)
 		{
-			_putchar(b);
-			
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
 	}
-		
+}

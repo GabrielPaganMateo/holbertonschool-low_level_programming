260eb7a428f170b6880ca5b100eef2d98dc551c9
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-05 13:25:18 +0000

    whitespace

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
  * print_last_digit- Prints last digit of a number
- * @n: last digit of number
+ * @n: number that is given
  * Return: value of last digit
  */
 int print_last_digit(int n)
 {
 	int x;
-	
+
 	x = n % 10;
 	
 	if (x < 0)
 	{
 		x = x * (-1);
 	}
-
 	_putchar('0' + x);
-
-	return(x);
+	return (x);
 }

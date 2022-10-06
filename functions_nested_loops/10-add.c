67eaaad94b34689a32e90890fe5ef5fe9dacd201
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-06 00:42:23 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add- Add two integers
+ *@a: first integer
+ *@b: second integer
+ *
+ * return: Result of the sum
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}

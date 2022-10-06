021893690642b3e15b2cfb1d46be530e48ba8b77
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-06 22:30:18 +0000

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers- Print numbers
+ * Return: 0 to 9, except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+		else
+		{
+
+		}
+	}
+	_putchar('\n');
+}

b6a08df242dc6c2d8601992a50f198f30e722ec1
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-06 22:55:13 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers- Prints numbers
+ * Return: 0 to 14, ten times
+ */
+void more_numbers(void)
+{
+	int times, n;
+	
+	for (times = 1 ; times <= 10 ; times++)
+	{
+		for (n = 0 ; n <= 14 ; n++)
+		{
+			_putchar(n + '0');
+		}
+			_putchar('\n');
+	}
+}

4fd2d97c9cedaf81816705063c54fdc1c759d5d1
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-06 18:54:00 +0000

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit- Checks for a digit
+ * @c: digit to be checked
+ * Return: 1 if c is digit, 0 if otherwise
+ */
+int _isdigit(int c);
+{
+	if (c <= '9' && c >= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ int main(void);
 int _putchar (char c);
 
 int _isupper(int c);
+
+int _isdigit(int c);

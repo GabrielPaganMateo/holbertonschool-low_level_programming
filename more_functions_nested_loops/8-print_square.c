c1b8c3b62e4b9e38cd19d250aaf457de841edfd9
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-07 12:32:55 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square- Prints a square
+ * @size: size of the square
+ * Return: # in form of square
+ */
+void print_square(int size)
+{
+	int width, length;
+
+	if (size > 0)
+	{
+		for (width = 0 ; width <= size ; width++)
+		{
+			for (length = 0 ; length <= size ; length++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}

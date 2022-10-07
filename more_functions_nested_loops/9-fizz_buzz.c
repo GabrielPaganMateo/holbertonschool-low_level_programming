82b304ede0407650162cfbf18b0945ea69a69893
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-07 13:02:38 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,11 @@ int main(void)
 		if ((num % 3) == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if ((num % 5) == 0)
 		{
 			printf("Buzz");
-			if (num < 99)
-			{
-				printf(" ");
-			}
+
 		}
 		else if ((num % 15) == 0)
 		{
@@ -29,9 +25,11 @@ int main(void)
 		else
 		{
 			printf("%i", num);
-			printf(" ");
 		}
-
+			if (num < 99)
+			{
+			printf(" ");
+			}
 	}
 	return (0);
 }

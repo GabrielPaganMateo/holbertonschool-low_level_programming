64ab62e8f8a4de2abcf31c36c89b3930077c90eb
Gabriel Pagán Mateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2022-10-07 22:25:34 -0400

    Update 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,35 +7,35 @@
  * Return: All Natural number between n & 98
  */
 void print_to_98(int n)
-{
-	if (n <= 98)
-	{
-		while (n <= 98)
-		{
+			{
+			if (n <= 98)
+			{
+			while (n <= 98)
+			{
 			if (n == 98)
 			{
-				printf("%i\n", n);
+			printf("%i\n", n);
 			}
 			else
 			{
-				printf("%i, ", n);
+			printf("%i, ", n);
 			}
 			n++;
-		}
-	}
-	else
-	{
-		while (n >= 98)
-		{
-			if (n == 98)
-			{
-				printf("%i\n", n);
+			}	
 			}
 			else
 			{
-				printf("%i, ", n);
+			while (n >= 98)
+			{
+			if (n == 98)
+			{
+			printf("%i\n", n);
+			}
+			else
+			{
+			printf("%i, ", n);
 			}
 			n--;
-		}
-	}
-}
+			}
+			}
+			}

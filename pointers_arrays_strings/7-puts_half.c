a85507e5b91db2fd8d3c6bf866558d4d86a663d2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 23:09:35 +0000

    puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - Prints second half of a string
+ * @str : string
+ * Return : string characters
+ */
+void puts_half(char *str)
+{
+	int length, i, l;
+
+	length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++
+	}
+	
+	l = length;
+
+	if (length % 2 == 0)
+	{
+		l = length / 2;
+	}
+	else 
+	{
+		l = (length - 1) / 2;
+	}
+
+	for ( i = l ; i < (l * 2) ; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}

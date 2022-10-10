6b56330b2007c9f086629c864c758d80c6dd19a0
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 20:35:55 +0000

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,28 +6,28 @@
  */
 void rev_string(char *s)
 {
-int i, j, k, l, length, rep;
-char temp;
+	int i, j, k, l, length, rep;
+	char temp;
 
-        length = 0;
+	length = 0;
 
-        while (*(s + length) != '\0')
-        {
-                length++;
-        }
-	
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
 	j = length - 1;
 	l = length / 2;
 	i = j;
 	k = 0;
 
-        for (rep = 0; rep < l ; rep++)
-	{	
+	for (rep = 0; rep < l ; rep++)
+	{
 		temp = *(s + k);
-               *(s + k) = *(s + i);
+		*(s + k) = *(s + i);
 		*(s + i) = temp;
 
 		k++;
 		i--;
-        }
+	}
 }

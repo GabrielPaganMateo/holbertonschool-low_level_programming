4ffeb23d1a62eac59d9942c23d0cb8351f6474f7
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 10:19:01 +0000

    new repository

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates value to 98 of int, by means of a pointer
+ * @*n : pointer to int
+ * Return: Updated int with value 98
+ */
+void reset_to_98(int *n);
+{

8d3722c9f3d6fc0a399f0611764f06feaca09cc7
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 17:05:53 +0000

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s : string
+ * Return : gnirts is string
+ */
+void rev_string(char *s)
+{
+int i, j;
+
+        j = 0;
+
+        while (*(s + j) != '\0')
+        {
+                j++;
+        }
+
+        j = j - 1;
+
+        for (i = j ; i >= 0 ; i--)
+        {
+                s[j] = (*(s + i));
+        }
+}

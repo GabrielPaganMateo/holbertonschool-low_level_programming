414abccbd541af5e640edd196e843bc632750cd8
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 14:38:21 +0000

    puts

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string
+ * Return: gnirts = string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	_strlen(*s);
+
+	for (i = _strlen(*s) ; i <= 0 ; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}	

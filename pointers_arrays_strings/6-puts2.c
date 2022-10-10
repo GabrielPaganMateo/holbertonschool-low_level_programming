af11e3bd5eb870c74c0f1805856cde5d12c8fea8
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 21:42:39 +0000

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,14 @@ void puts2(char *str)
 {
 	int length, i;
 
-	while (*str != '\0')
+	while (*(str + length) != '\0')
 	{
 		length++;
 	}
 	
 	for (i = 0 ; i <= length ; i++)
 	{
-		if (i % 2 == 0)
+		if ((i % 2) == 0)
 		{
 			_putchar(*(str + i));
 		}

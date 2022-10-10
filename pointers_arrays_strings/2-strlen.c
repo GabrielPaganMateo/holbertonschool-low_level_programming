1f882b731017a79782dfcc1d398251f5484d6bfc
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 12:01:33 +0000

    string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @s : string 
+ * Return: value of string length
+ */
+int _strlen(char *s);
+{
+	int count = 0;
+
+	while (str[count] != '\0') 
+	{
+		count++
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-int _putchar(char c);
+int main(void);
 
 void reset_to_98(int *n);
 

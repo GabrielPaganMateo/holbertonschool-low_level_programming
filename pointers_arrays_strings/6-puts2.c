9b71951a59b06fd08fd984f066cb4f1e7e82953d
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 20:55:10 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - Prints every character of a string
+ * @str : string
+ * Return : characters of string
+ */
+void puts2(char *str)
+{
+	int length, i;
+
+	while (*str != '\0')
+	{
+		length++
+	}
+	
+	for (i = 0 ; i <= length ; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}

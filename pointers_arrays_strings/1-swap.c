19db7770251a4bdd6112912600d324088d8e1536
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-10 11:12:13 +0000

    first push

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_init - swaps the value of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Sum of both integers
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}

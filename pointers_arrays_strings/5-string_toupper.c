4f624150dcfa1608640a8cd0f8dadf24efd015c7
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 23:20:52 +0000

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *string_toupper - Changes all lowercase letters of string to uppercase
+ * @*: string
+ * Return: uppercase characters
+ */
+char *string_toupper(char *str);
+{
+	int i, length;
+
+	while (*(str + length) != '\0')
+	{
+		length++
+	}
+
+	for (i = 0 ; i < length ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+}

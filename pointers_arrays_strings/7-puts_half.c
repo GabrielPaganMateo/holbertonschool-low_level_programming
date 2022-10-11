69618d5539d01b4ef897f1746210b39353ed597c
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 00:28:08 +0000

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -25,18 +25,9 @@ void puts_half(char *str)
 	{
 		l = (length - 1) / 2;
 	}
-	
-	if (l < 20)
-	{
-	for (i = l ; i < (l * 2)  ; i++)
-	{
-		_putchar(*(str + i));
-	}
-	}
-	else
-	for (i = l ; i < ((l * 2) - 1) ; i++)
-	{
-		_putchar(*(str + i));
-	}
+		for (i = l ; i < (l * 2)  ; i++)
+		{
+			_putchar(*(str + i));
+		}
 	_putchar('\n');
 }

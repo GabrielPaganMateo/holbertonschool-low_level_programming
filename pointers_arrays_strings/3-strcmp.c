709bf57db1989ee5ed0dc6860c8ff29c064d42f7
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 21:16:17 +0000

    cmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,30 +7,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, length;
-	char *comp1, *comp2;
+	int i;
 
-	while (s1[length] != '\0')
+	while(s1[i] == s2[i] && s1[i] == '\0')
 	{
-		length++;
+		i++
 	}
 
-	for (i = 0; ; i++)
+	if (s1[i] < s2[i])
 	{
-		*(comp2 + i) = s1[i];
-		*(comp2 + i) = s2[i];
-	
-		if (comp1[i] != comp2[i])
-		{
-			if (comp1[i] < comp2[i])
-			{
-				return (-1);
-			}
-			if (comp1[i] > comp2[i])
-			{
-				return (1);
-			}
-		}
+		return (-1);
 	}
-	return (0);
+	else if (s1[i] > s2[i])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

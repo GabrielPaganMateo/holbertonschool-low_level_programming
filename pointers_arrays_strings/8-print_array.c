9967d4df7c017bde820e12a9283cbd4895ac6460
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 01:15:22 +0000

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a : array
+ * @n : quantity of elements
+ * Return : array
+ */
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%i, ", *(a + i);
+	}
+}

b22d2a777b20a513efb63aa2d7cf45a74419a07b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 19:57:51 +0000

    strn

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncat - concatenate two strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of elements from second string
+ */
+char *strncat(char *dest, char *src, int n)
+{
+	int dlength, i;
+
+	while (dest[dlength] != '\0')
+	{
+		dlength++
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dlength + i] = src[i];
+	}
+	dest[dlength + i] = '\0';
+	return (dest);
+}

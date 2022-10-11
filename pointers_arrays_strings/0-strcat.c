feb57a9d513d8fe3d7f1ce7704558ad60f7fa576
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 16:54:38 +0000

    concatenate

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,15 +14,16 @@ char *_strcat(char *dest, char *src)
 
 	while (*(dest + length1) != '0')
 	{
-		length1++
+		length1++;
 	}
 	while (*(src + length2) != '0')
 	{
-		length2++
+		length2++;
 	}
 
 	for (i = 0 , j = length1; i < (length1 + length2) ; i++ , j++)
 	{
 		*(dest + i) = *(src + j)
 	}
+	return (dest);
 }

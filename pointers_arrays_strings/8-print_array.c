b63f420d57e8009dad1fdfc31314560dca1ec3cf
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-11 01:20:48 +0000

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i <= n ; i++)
 	{
+		if (i == n)
+		printf("%i", *(a + i));
+		else
 		printf("%i, ", *(a + i));
 	}
 }

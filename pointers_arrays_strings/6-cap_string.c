c8666c3ef05fcf9138a7cb6257ab792e67d2aad4
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-12 14:15:57 +0000

    ca

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				str[i] = str[i] - 32
+				str[i] = str[i] - 32;
 			}
 		}
 		else if (i == '"' || i == ' ' || i == '.' || i == '\n' || i == '!'
@@ -22,10 +22,10 @@ char *cap_string(char *str)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				str[i] = str - 32
+				str[i] = str - 32;
 			}
 			continue;
-		}				
+		}
 	}
 	return (str);
 }

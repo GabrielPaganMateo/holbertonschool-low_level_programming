a6f257dea1ac140a4660b59ccc94a6e807f2ab68
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-12 11:38:29 +0000

    cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * *cap_string - Capitalizes all words of a string
+ * @str: String
+ * Return: String
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if(str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+			++i
+			if(str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		if (str[i] == '.')
+		{
+			++i
+			if(str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+	}
+	return (str);
+}
+

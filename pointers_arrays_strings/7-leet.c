9d8b30337c74f58e835be888040090395990f97b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-12 18:50:38 +0000

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,23 +6,5 @@
  */
 char *leet(char *str)
 {
-	int i, j;
-	j = 0;
-	
-	char let[10] = {'A', 'E', 'O', 'T', 'L', 'a', 'e', 'o', 't', 'l', '\0'};
-	char num[5] = {'4', '3', '0', '7', '1', '\0'};
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] == let[0] || str[i] == let[1] 
-		|| str[i] == let[2] || str[i] == let[3] 
-		|| str[i] == let[4] || str[i] == let[5]
-		|| str[i] == let[6] || str[i] == let[7]
-		|| str[i] == let[8] || str[i] == let[8]
-		|| str[i] == let[9])
-		{
-			str[i] = num[j];
-		}
-	}
 	return (str);
 }

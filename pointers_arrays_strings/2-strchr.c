9fc75acf3cb79ae6f4ffcb5acd27bd2384a1b418
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-13 13:19:14 +0000

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,14 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
-	{
-		s[i];
-		
+	{	
 		if (s[i] == c)
 		{
-			return (*s[i]);
+			return (s[i]);
 		}
 		else
 		{
-			*_strchr(NULL);
+			*_strchr('\0');
 		}
 	}
 }

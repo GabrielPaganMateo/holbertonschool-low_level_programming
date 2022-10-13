2f3f90cdfc8b79c2be64ce34d873c7e2812f69c2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-13 12:48:33 +0000

    0

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memcpy - copies n bytes from memory area src to memory area dest
+ * *_memcpy - copies n bytes from memory area src to memory area dest
  * @src: source memory area
  * @dest: destination memory area
  * @n: number of bytes

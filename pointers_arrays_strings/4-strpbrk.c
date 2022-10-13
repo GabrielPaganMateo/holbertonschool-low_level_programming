c3e24dd60e776fc0cb9269715440adfdfd8ae4ef
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-13 23:24:08 +0000

    4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -7,21 +7,12 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
 
-	for (i = 0; s[i] != '\0' ; i++)
-	{
-		for (j = 0; accept[j] != '\0' ; j++)
-		{
-			if(s[i] == accept[j])
-			{
-				return (s);
-			}
-		}
-		if (s[i] != accept[j])
-		{
-			return (s);
-		}	
-	}
-	return (s);
+        do {
+                if (*s == *accept)
+                {
+                        return (s);
+                }
+        } while (*s++);
+        return (0);
 }

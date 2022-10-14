60f7dae218ccb71692768351f495e146b9a6965e
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-14 00:15:30 +0000

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: Pointer to beginnig of located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	do {
+		if (*needle == *haystack)
+		{
+			return (needle);
+		}
+	} while (haystack++);
+	return (0);
+}

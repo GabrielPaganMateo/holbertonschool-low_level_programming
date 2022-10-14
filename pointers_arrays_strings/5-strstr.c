607923a260dca5bdf0e099d16a21c33c7c5f36af
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-14 11:16:03 +0000

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,16 +7,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int i, j;
 
-	do { 
-		for (i = 0 ; haystack[i] != '\0' ; i++)
-		{	
-			if (*needle == *haystack)
-			{	
-				return (needle);
+	j = 0;
+
+	for (i = 0 ; haystack[i] != '\0' ; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			for (j = 0 ; needle[j] != '\0' ; j++)
+			{
+				if (haystack[i] == needle[j])
+				{
+					return (needle);
+				}
 			}
 		}
-	} while (haystack++);
+	}
 	return (0);
 }

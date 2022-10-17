2bab8c10a0c5a23a7ed974605f7ae3d0a6d6cd6c
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-17 22:44:54 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,14 +3,20 @@
  * is_prime_number - determines if integer is prime number,
  * if yes then return 1 otherwise return 0
  * @n: given number
+ * @i: static variable
+ * Return: integer
  */
 int _prime(int i, int n);
 int is_prime_number(int n)
 {
 	return (_prime(2, n));
-	
 }
-
+/**
+ *_prime - determin if the number is prime
+ *@n: given number
+ *@i: static variable
+ *Return: integer, if 1 is prime, 0 otherwise
+ */
 int _prime(int i, int n)
 {
 	if (n == 0 || n == 1)
@@ -19,12 +25,11 @@ int _prime(int i, int n)
 	}
 	else if (n == i)
 	{
-        return (1);
+		return (1);
 	}
 	else if (n % i == 0)
 	{
-        return (0);
+		return (0);
 	}
-    return _prime(i + 1, n);
+	return (_prime(i + 1, n));
 }
-

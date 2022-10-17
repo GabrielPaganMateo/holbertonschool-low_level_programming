d672545cb9f9a2d085b445b65b4d457d6ce62a00
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-17 14:48:34 +0000

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _powr_recursion - returns the value of x raised to the power of y
+ * @x: base
+ * @y: power
+ * @Return: integer
+ */
+int _pow_recursion(int x, int y);
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return(_pow_recursion(x, y - 1))
+	}
+}
+

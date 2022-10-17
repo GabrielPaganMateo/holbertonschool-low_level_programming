20c1a3cd514358adbbdd06aacf683d4897d39904
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-17 13:07:52 +0000

    1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
  * Return: characters
  */
 void _print_rev_recursion(char *s)
-{	
+{
 	if (*s == '\0')
 	{
 		return;
@@ -13,7 +13,7 @@ void _print_rev_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }

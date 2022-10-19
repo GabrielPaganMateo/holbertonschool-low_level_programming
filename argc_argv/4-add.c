b24df22238e06941b3d8026e932a017d26795262
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-19 22:39:40 +0000

    Defined and Compiled successfully 4-add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,22 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - add positive numbers
  * @argc: argument counter
  * @argv: argument array
  * Return: integer
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	int i, j, zero;
+	int i, sum, zero, num;
+	char Error[] = "Error";
+	sum = 0;
 
-	zero = 0;
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
 
 	for (i = 0 ; i < argc ; i++)
 	{
-		num = atoi(argv[i]);
-		zero += num;
+		if (isdigit(argv[i]) != 0)
+		{
+			num = atoi(argv[i]);
+			zero += num;
+		}	
+		else
+		{
+			printf("%s\n", Error);
+			return (1);
+		}
 	}
-	printf("%d", zero);
 	return (0);
 }

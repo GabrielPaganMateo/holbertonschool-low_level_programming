f52910938f7c317abfe8fd88a8619fec8062c977
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-19 14:24:10 +0000

    Define the function within 0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * myname - Program that prints its name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Integer
+ */
+int myname(int argc, char *arg[])
+{
+	printf("%s\n", argv[0]);
+}

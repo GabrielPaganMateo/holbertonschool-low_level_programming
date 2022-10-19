5de0750efb8d13b7af4b42990e73d02b3e8825df
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-19 17:54:31 +0000

    Changed if condtion to == 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 	int product, num1, num2;
 	char error[] = "Error";
 
-	if (argc < 2 && argc != 0 && argc != 1)
+	if (argc == 3)
 	{
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
 
-	product = num1 * num2;
+		product = num1 * num2;
 
-	printf("%d\n", product);
+		printf("%d\n", product);
 	}
 	else
 	{

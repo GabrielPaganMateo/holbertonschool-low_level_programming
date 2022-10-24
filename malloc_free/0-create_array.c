424dd523820532c5c462d8a90dfe3d1081214762
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-24 11:42:24 +0000

    Add various changes to 0-create_array for correct output

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - Creates an array of chars, and initializes it with a specific char
+ * create_array - Creates an array of chars, and
+ * initializes it with a specific char
  * @size: size of the allocated array
  * @c: specified character
  * Return: A block that has been partially allocated by characters
@@ -9,15 +10,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return ('\0');
 	}
 
-	array =malloc((size + 1) * sizeof(char));
-	
-		array[0] = c;
+	array = malloc((size) * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 	
 	return (array);
 }

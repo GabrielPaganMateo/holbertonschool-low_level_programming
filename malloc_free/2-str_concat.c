1803005affeb124fd7c34f008d47a0c270678746
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-24 15:10:08 +0000

    Switched edge cases

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,13 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int totalLength, length1, length2, i;
 	char *concatenated;
 
-	if (s1 == NULL)
-	{
-		return (s2);
-	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		return (s1);
+	}
+	else if (s1 == NULL)
+	{
+		return (s2);
 	}
 	else if (s1 == NULL && s2 == NULL)
 	{

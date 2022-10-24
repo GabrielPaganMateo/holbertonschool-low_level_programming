08e39e0b314edfbeebc23afd3ae710506636d226
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-24 16:10:14 +0000

    Edge case

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+int checkNULL(char *s);
 /**
  * str_concat - Function that concatenates two strings
  * @s1: First string
@@ -13,21 +14,8 @@ char *str_concat(char *s1, char *s2)
 	int totalLength, length1, length2, i;
 	char *concatenated;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
-	else if (s2 == NULL)
-	{
-		return (s1);
-	}
-	else if (s1 == NULL)
-	{
-		return (s2);
-	}
-	
-	length1 = strlen(s1);
-	length2 = strlen(s2);
+	length1 = checkNULL(s1);
+	length2 = checkNULL(s2);
 	totalLength = length1 + length2;
 
 	concatenated = malloc((totalLength + 1) * sizeof(char));
@@ -49,4 +37,17 @@ char *str_concat(char *s1, char *s2)
 	concatenated[length1 + i] = '\0';
 
 	return (concatenated);
+}
+/**
+ * checkNULL - checks if string is NULL, if true return 0, else return strlen
+ * @s: string
+ * Return: return int
+ */
+int checkNULL(char *s)
+{
+	if (s == NULL)
+	{
+		return(0);
+	}
+	return (strlen(s));
 }

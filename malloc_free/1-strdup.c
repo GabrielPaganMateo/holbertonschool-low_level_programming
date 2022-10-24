8c1b9fa46099bc8a2e7d000c1a93d3f297001e6b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-24 12:10:43 +0000

    Add .c with function _strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: given string
+ * Return: pointer to a string
+ */
+char *_strdup(char *str)
+{
+	char *string;
+	int length;
+
+	while(length != '\0')
+	{
+		length++;
+	}
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	string = malloc ((length + 1) * sizeof(char));
+	
+	if (string == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (length + 1); i++)
+	{
+		string[i] = str[i];
+	}
+}

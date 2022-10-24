c016b7c8395ab8a3a6a260989a83e269f0dd1f25
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-24 11:50:13 +0000

    Checking malloc's return

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * create_array - Creates an array of chars, and
  * initializes it with a specific char
@@ -18,6 +19,11 @@ char *create_array(unsigned int size, char c)
 	}
 
 	array = malloc((size) * sizeof(char));
+
+	if (array == NULL)
+	{
+		return('\0');
+	}
 
 	for (i = 0; i < size; i++)
 	{

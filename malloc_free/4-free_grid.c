bdc4eab6b617d25cbefcab7f60f811878a375c0b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-25 00:57:52 +0000

    Created a function for free-grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_grid - function that frees a 2 dimensional array previously created
+ * @grid: two dimensional grid
+ * @height: amount of rows of array
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height ; i > 0 ; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

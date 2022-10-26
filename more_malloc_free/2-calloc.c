46b4da1ac207a309fbc098b7a3535221c460a259
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-26 15:24:26 +0000

    Added returned to initial code

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,39 +9,24 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *intalloc, i;
-	char *charalloc;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	if(size == 1)
-	{
 	intalloc = malloc(nmemb * size);
+
 	if (intalloc == NULL)
 	{
 		return (NULL);
 	}
-	else
+
 	{
 	for (i = 0; i < nmemb; i++)
 	{
 		intalloc[i] = 0;
 	}
-	return (intalloc);
-	}
-	}
-	else
-	charalloc = malloc(nmemb * size);
-	if (charalloc == NULL)
-	{
-		return (NULL);
-	}
 
-	for (i = 0; i < nmemb; i++)
-	{
-		charalloc[i] = 0;
-	}
-	return (charalloc);
+	return (intalloc);
 }

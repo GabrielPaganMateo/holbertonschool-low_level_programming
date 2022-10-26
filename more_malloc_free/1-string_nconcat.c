e9bc32c138cc4a12004a37dbf1344bef3c276b76
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-26 12:36:39 +0000

    Fix malloc again

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = length2;
 	}
 
-	concat = malloc(sizeof(char) * (n));
+	concat = malloc(sizeof(char) * (n + length1));
 	if (concat == NULL)
 	{
 		return (NULL);
@@ -46,7 +46,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		concat[i] = s1[i];
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		concat[length1 + i] = s2[i];
 	}

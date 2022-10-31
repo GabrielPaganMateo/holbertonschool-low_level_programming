f9339849f6fe86335e077692c5c95f1290d7be7b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-31 12:13:00 +0000

    Fix coding style

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 	}
 	else if (d->name == NULL && d->owner == NULL)
 	{
-		printf ("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
+		printf("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
 	}
 	else
 	{
-		printf ("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }

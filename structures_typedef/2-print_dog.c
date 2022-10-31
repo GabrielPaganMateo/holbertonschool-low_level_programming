103aa214c3263344bc15885b57449d81d42842bb
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-31 12:06:02 +0000

    Add newline

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf ("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+		printf ("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }

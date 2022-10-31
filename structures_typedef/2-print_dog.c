849292b7262a4166c875a9f31cf04f20b4c19bd2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-31 12:12:12 +0000

    Add edge case

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,6 +11,10 @@ void print_dog(struct dog *d)
 	{
 
 	}
+	else if (d->name == NULL && d->owner == NULL)
+	{
+		printf ("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
+	}
 	else
 	{
 		printf ("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);

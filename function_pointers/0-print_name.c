0afb7a9e6f81fee73571ea709a52116ee7937670
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-31 23:06:44 +0000

    Add print name function pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers"
+/**
+ * print_name - Function that prints a name
+ * @name: name
+ * @f: function pointer
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*fptr)(char);
+
+	fptr = &f;
+
+	(*fptr)(name);
+}

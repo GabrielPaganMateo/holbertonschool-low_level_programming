dff5fa87c081904aef3ba0ebe122e1371ed7582c
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-10-31 11:27:34 +0000

    Fix if else statement

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,11 +14,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 	{
 		printf("Ok");
-
-		return (NULL);
 	}
-
+	else
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }

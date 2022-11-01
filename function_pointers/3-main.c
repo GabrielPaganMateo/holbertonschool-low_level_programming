e9d532e64226b2aec7f2fa94cc7ba6fb5e98ed89
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 17:13:09 +0000

    Add main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,14 +3,17 @@
 /**
  * main - Accept arguments for operation
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	int a, b;
-	char s;
+	char *s;
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	s = argv[2];
+
+	return (get_op_func(s)(a, b));
+}
 
 
 

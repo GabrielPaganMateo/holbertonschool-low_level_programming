2261c566d7e04603ea785e4e0ada41cbbf59e6b7
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 18:31:30 +0000

    Calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,19 +4,36 @@
 /**
  * main - Accept arguments for operation
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b, c;
 	char *s;
+
+	if (argc < 0 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	s = argv[2];
 
+	if (s != '+' || s != '-' || s != '/' || s != '%' || s != '*')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	c = get_op_func(s)(a, b);
 
 	printf("%d\n", c);
-
 
 	return (0);
 }

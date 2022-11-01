eadf0fe8307926ac0f8a0af1359fdbbb2401f9c8
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 10:23:42 +0000

    Fix errors

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,6 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int (*fptr)(int);
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+
 	if (size <= 0)
 	{
 		return (-1);

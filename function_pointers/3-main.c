4a39019803868a978ed6fd7cec68abc6a4622c30
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 18:14:23 +0000

    Add calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,18 +1,24 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Accept arguments for operation
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-	int a, b;
+	int a, b, c;
 	char *s;
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	s = argv[2];
 
-	return (get_op_func(s)(a, b));
+	c = get_op_func(s)(a, b);
+
+	printf("%d\n", c);
+
+
+	return (0);
 }
 
 

73a0941488b1b703563aca3cf1183969e2667d1f
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 00:04:28 +0000

    Add array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to function
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	void (*fptr)(int);
+
+	fptr = action;
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*fptr)(array[i]);
+	}
+}

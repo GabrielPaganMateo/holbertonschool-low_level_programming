9c3ba1f5f563dc55a21d1b060c283bd0d6949260
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-01 19:06:43 +0000

    Calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,35 +9,34 @@ int main(int argc, char *argv[])
 	int a, b, c;
 	char *s;
 
-	if (argc > 4 || argc < 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	s = argv[2];
-	
-	if (*s != '+' || *s != '-' || *s != '/' || *s != '%' || *s != '*')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if (argv[3] == 0)
+	if (argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	c = get_op_func(s)(a, b);
 
-	printf("%d\n", c);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = argv[2];
+
+	if (*s == '+' || *s == '-' || *s == '/' || *s == '*' || *s == '%')
+	{
+		c = get_op_func(s)(a, b);
+
+		printf("%d\n", c);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	return (0);
 }
-
-
-
-

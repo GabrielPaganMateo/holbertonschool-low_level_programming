1901e97eb62e6637146db8caf3e9cc1c77a82fdc
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-02 19:20:48 +0000

    Print all documentation

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - Prints anything
+ * @format: list of data types passed to function
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+

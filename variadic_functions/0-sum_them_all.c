a0ef041acfe8395b374e150b896f28f98b866301
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-02 14:11:16 +0000

    sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,8 +6,24 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	int i, sum;
+	sum = 0;
 
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+
+	va_end(ptr);
+
+	return (sum);
+}

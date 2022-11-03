54e92d72f2dc5c6af1c52eb1926cf29165829a34
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-03 17:39:54 +0000

    Checker test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * print_all - Prints anything
  * @format: list of data types passed to function
@@ -7,8 +8,31 @@
  */
 void print_all(const char * const format, ...)
 {
-	while (format)
+	char *s;
+	int i, c;
+	double d;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
 	{
-		printf("format");
+		if (*format == 'd')
+		{
+			i = va_arg(args, int);
+			printf("%d\n", i);
+		}
+		else if (*format == 'c')
+		{
+			j = va_arg(args, int);
+			printf("%c", c);
+		}
+		else if (*format == 'f')
+		{
+			d = va_arg(args, double);
+			printf("%f", d);
+		}
+		++format;
 	}
+	va_end(args);
 }

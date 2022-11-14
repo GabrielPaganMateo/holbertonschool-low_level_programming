85f4ea50775f2ecc1f40442044335c00ecbe939a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-14 11:30:50 +0000

    Add list_len function file

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - gives number of elements in a linked list
+ * @h - pointer to head of list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int NumOfNodes = 0;
+
+	while (h != NULL)
+	{
+		NumOfNodes = NumOfNodes + 1;
+		h = h->next;
+	}
+	return (NumOfNodes);
+}

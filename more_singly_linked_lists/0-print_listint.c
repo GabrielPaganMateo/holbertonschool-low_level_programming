73824bb793d47c33d57c66f2d0bd14008fdae67e
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-14 17:07:55 +0000

    Add print int list

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *print_listint - prints all the elements of a listint_t
+ *@h - pointer to header
+ *Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int NumOfNodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		NumOfNodes += 1;
+	}
+	return (NumOfNodes);
+}
+
+

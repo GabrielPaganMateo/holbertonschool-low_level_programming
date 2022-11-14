6e46b8f9df9dded3db7f13a9960858c5609c7b47
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-14 23:10:48 +0000

    Fix free list function

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,8 @@ void free_listint2(listint_t **head)
 
 	while (head != NULL)
 	{
-		temp = &head;
-		&head = head->next;
-		free (temp);
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
-		head = NULL;
 }

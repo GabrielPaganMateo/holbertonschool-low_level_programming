9ccc94d3e9a07927b1d4fcdd24871e3217a56125
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-14 14:30:36 +0000

    Finally FREE

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,12 +5,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t* temp;
+	list_t *temp;
 
 	while (head != NULL)
 	{
 		temp = head;
-		head = head->next;
+		head = temp->next;
+		free(temp->str);
 		free(temp);
 	}
 }

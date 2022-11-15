e6454b68222c610bcd1238f7f4cd046edefeba1d
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-15 14:29:16 +0000

    Add insert node function

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i != (idx - 1) || temp != NULL)
 	{
-		temp = tmp->next;
+		temp = temp->next;
 		temp2 = temp2->next;
 		i++;
 	}

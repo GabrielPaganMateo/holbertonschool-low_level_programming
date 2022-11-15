951d1f9c5a3e2f211323a3fde12322861414f97a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-15 18:17:35 +0000

    Change

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,11 +22,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	NewNode->n = n;
 
-	if (idx == '\0')
-	{
-		return (NULL);
-	}
-
 	if (idx == 0)
 	{
 		NewNode->next = *head;

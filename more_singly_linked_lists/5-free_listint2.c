6beb065933ca9dbf6b697f7d219f1bc587d1c95a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-15 10:52:15 +0000

    Finally freed

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -5,13 +5,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t **temp;
+	listint_t *temp;
 
 	if (head)
 	{
-		while (head != NULL)
+		while (*head != NULL)
 		{
-			temp = head;
+			temp = *head;
 			*head = (*head)->next;
 			free(temp);
 		}

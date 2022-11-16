513a0246fb25ff662c9e3ef75aa41bb536310f9a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-16 14:30:01 +0000

    Add files for task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to string of 0 and 1 chars
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int uint = 0;
+	int i;
+	int len = strlen(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		uint <<= 1;
+		uint += b[i] - '0';
+		if (i = len)
+		{
+			return (uint);
+		}
+		i++;
+	}
+	return (uint);
+}

52aec7e1a162a3d7b3f3db26e7f9a6808a4e7f91
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-18 14:49:04 +0000

    Add create file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,15 +15,18 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (fd == -1)
 	{
 		fd = open(filename, O_TRUNC);
 	}
 
-
-	write(fd, text_content, (len));
+	if (text_content != NULL)
+	{
+		write(fd, text_content, (len));
+		close(fd);
+	}
 
 	close(fd);
 

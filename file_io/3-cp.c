adb66b77c3a6d0b3af63e4b5d2e8b3ee044a754b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-18 18:56:12 +0000

    Add cp program

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,10 +31,14 @@ int main (int argc, char *argv[])
 		exit(99);
 	}
 
-	while (argv[1] != EOF)
+	while (1)
 	{
 	readcount = read(fd1, buf, 1024);
 	write(fd2, buf, readcount);
+		if (feof(fd1))
+		{
+			break;
+		}
 	}
 
 	cl1 = close(fd1);

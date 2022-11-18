61dc6a43fa707817ce0d69f1a9bda095a01c31ff
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-18 14:56:30 +0000

    Add edge case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len = strlen(text_content);
+	int len;
 
 	if (filename == NULL)
 	{
@@ -19,8 +19,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
+		len = strlen(text_content);
 		write(fd, text_content, (len));
 	}
+
 
 	close(fd);
 

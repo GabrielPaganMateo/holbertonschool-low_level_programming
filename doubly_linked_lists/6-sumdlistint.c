1b857857740fe63863a12a993b6448ea24276b09
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-21 13:59:41 +0000

    Add sum function

---
+++ doubly_linked_lists/6-sumdlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums all the data of a doubly linked list
+ * @head: pointer to head
+ * Return: sum of data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	while (temp->next != NULL)
+	{
+		sum += temp->data;
+	}
+
+	return (sum);
+}

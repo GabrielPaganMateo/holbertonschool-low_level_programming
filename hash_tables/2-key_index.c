84866187454d72c33552ec29398e0a2003fd3b2a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-23 11:53:23 +0000

    Add find index function

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @size: size of the array
  * Return: index
  */
-unsigned long int key_index(const unsigned char *key, __unused__((attribute)) unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int __unused__((attribute)) size)
 {
 	unsigned long int index;
 

3b02138cb5678eed041399e2ac5864151d4d213a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-23 11:21:53 +0000

    Fix create hash table function

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,19 +9,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	hash_table_t *TABLE = malloc(sizeof(hash_table_t));
+
 	if (TABLE == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	TABLE->size = size;
 
-	TABLE->array = malloc(sizeof(hash_node_t) * TABLE->size);
+	TABLE->array = malloc(sizeof(*hash_node_t) * TABLE->size);
 
 	for (i = 0; i < TABLE->size; i++)
 	{
 		TABLE->array[i] = NULL;
 	}
 
-	return(TABLE);
+	return (TABLE);
 }

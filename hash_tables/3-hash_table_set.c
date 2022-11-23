7d247327d5f49743d776894e422b6eb3364b673b
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-23 18:08:58 +0000

    Add hash table function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->value = strdup(value);
-	if (node->value = NULL)
+	if (node->value == NULL)
 	{
 		free(node->value);
 		return (0);

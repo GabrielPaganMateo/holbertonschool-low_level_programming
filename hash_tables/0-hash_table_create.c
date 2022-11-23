9c1c8dda008df288603828551212b9e2b6930e99
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-23 11:01:32 +0000

    Create hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t TABLE;
+
+	TABLE->size = size;
+
+	return(TABLE);
+}

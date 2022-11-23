67b5df652a5c7ac91082a326a45056413be7ce90
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-23 22:09:25 +0000

    Add hash table function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
-	while(node != NULL);
+	while (node != NULL);
 	{
 		if (strcmp(node->key, key) == 0)
 		{

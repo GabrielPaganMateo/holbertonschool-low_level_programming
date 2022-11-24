57d03aca3b1834f79344d7f83f9db72a39274ed2
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-24 00:48:56 +0000

    Print table function

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,13 +5,10 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *temp;
 	unsigned long int i;
 
 	if (ht == NULL)
 		return;
-
-	temp = ht->array;
 
 	for (i = 0; i < ht->size; i++)
 	{

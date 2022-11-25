60880789f6c226734281acacedbcd943b2dbae45
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2022-11-25 11:50:15 +0000

    Add free

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -6,32 +6,14 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *temp, *temp2;
-
-	temp = ht->array[0];
-	temp2 = ht->array[0];
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
-		{
-			if (ht->array[i]->next == NULL)
-			{
-				free(ht->array[i]);
-			}
-			else if (ht->array[i]->next != NULL)
-			{
-				temp = ht->array[i];
-
-				while (temp)
-				{
-					temp2 = temp;
-					temp = temp->next;
-					free(temp2->key);
-					free(temp2->value);
-					free(temp2);
-				}
-			}
-		}
+		free(ht->array[i]->key);
+		free(ht->array[i]->value);
+		free(ht->array[i]->next);
+		free(ht->array[i]);
 	}
+	free(ht->array);
+	free(ht);
 }
